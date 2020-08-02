19d1f4a6bb2da0cc27214ae4474ec8dca485a000
Luis <luisvargas.tbd@gmail.com>
2020-08-02 19:54:12 -0300

    doing few changes using Jquery

--- codigo.js
+++ codigo.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
   $("#hide").click(function(){
-    $(".jq").fadeOut();
+    $(".jq").fadeOut(1000);
   });
   $("#show").click(function(){
-    $(".jq").fadeIn();
+    $(".jq").fadeIn(1000);
   });
+  $("#move").click(function(){
+$("#move").animate({"opacity": "0.5", "margin-left": "20px"}, 1000).animate({"opacity":"1","margin-left":"0px"}, 1000);
+
+  })
 });
